fb2e118d24959b727cabc515dadecfff8590cb50
Gintare B <gintaar3@gmail.com>
2021-07-28 15:47:24 +0200

    title responsiveness

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='home'>
       <div className='home_authors'>
-        <h1>Trending Authors</h1>
+        <h1 className='home_authors_title'>Trending Authors</h1><div className='home_authors_line'></div>
         <AuthorsList/>
       </div>
     </div>
