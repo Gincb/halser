9070f8429d613b9ba499bb58f792b89654080d80
Gintare B <gintaar3@gmail.com>
2021-08-03 15:13:38 +0200

    view profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -9,20 +9,23 @@ function Profile() {
   const { currentUser } = useContext(AuthContext);
   const [editMode, setEditMode] = useState<boolean>(false);
   const [profileInformation, setProfileInformation] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     getUserDoc(app.auth().currentUser).then(data => {
       setProfileInformation(data);
+      setLoading(false)
     });
   }, []);
 
   return (
     <div className="profile">
-      {editMode ? (
+      {loading ? 'loading...' :
+      (editMode ? (
         <EditProfileCard setEditMode={setEditMode} profileInfo={profileInformation}/>
       ) : (
         <ProfileCard setEditMode={setEditMode}  profileInfo={profileInformation}/>
-      )}
+      ))}
     </div>
   )
 }
